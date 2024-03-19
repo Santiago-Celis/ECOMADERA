af28cc357002ac3446ddf520a1645ca550182033
Santiago Celis <celisgil.santiago.101@gmail.com>
2024-03-19 00:30:30 -0500

    FINAL

--- ecomadera.sql
+++ ecomadera.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `ecomadera` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ecomadera`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ecomadera
@@ -259,4 +261,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-18 10:53:22
+-- Dump completed on 2024-03-19  0:27:58
